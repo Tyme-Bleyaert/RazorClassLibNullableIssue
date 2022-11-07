ddc6ca5d63d01fd001d6bcc6720470a195e08cad
Tyme Bleyaert <tymeb@dja.be>
2022-11-07 14:52:59 +0100

    Initial Commit

---
+++ NullableWarningsIssue/Interfaces/ITest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableWarningsIssue.Interfaces
+{
+    internal interface ITest<T>
+    {
+        void Test();
+    }
+}
